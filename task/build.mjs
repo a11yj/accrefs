41cd5d5f8c9aa9d631ddd6f3fe593b73904a1f02
oti <otiext@gmail.com>
2023-05-02 13:23:56 +0900

    refactor: task/build.mjs, simplify

--- task/build.mjs
+++ task/build.mjs
@@ -3,7 +3,4 @@ import { database } from "./database.mjs";
 import { html } from "./html.mjs";
 import { matters } from "./matters.mjs";
 
-await assets()
-  .then(async () => await matters())
-  .then(async (matters) => await database(matters))
-  .then(async (db) => await html(db));
+await assets().then(matters).then(database).then(html);
