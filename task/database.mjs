588f112f13309474c7429a84ac053d80e12b5d2f
oti <otiext@gmail.com>
2023-05-02 13:23:27 +0900

    refactor: task/database.mjs, simplify

--- task/database.mjs
+++ task/database.mjs
@@ -1,17 +1,13 @@
 import { marked } from "marked";
 import { CONSTANTS } from "../site.config.mjs";
 
-export const database = async (matter) => {
-  const references = matter
+export const database = (matter) => ({
+  references: matter
     .filter((item) => !item.data.ignore)
     .map(({ content, data, id }) => ({
       data,
       id,
       marked: marked.parse(content),
-    }));
-
-  return Promise.resolve({
-    references,
-    ...CONSTANTS,
-  });
-};
+    })),
+  ...CONSTANTS,
+});
