64046ac7ed869f685ebbd49444ae7d8d74f694aa
oti <otiext@gmail.com>
2023-05-11 12:12:20 +0900

    refactor: task/database.mjs, `references: []`

--- task/database.mjs
+++ task/database.mjs
@@ -2,12 +2,14 @@ import { marked } from "marked";
 import { CONSTANTS } from "../site.config.mjs";
 
 export const database = (matter) => ({
-  references: matter
-    .filter((item) => !item.data.ignore)
-    .map(({ content, data, id }) => ({
-      data,
-      id,
-      marked: marked.parse(content),
-    })),
+  references: matter.flatMap(({ content, data, id }) =>
+    !data.ignore
+      ? {
+          data,
+          id,
+          marked: marked.parse(content),
+        }
+      : []
+  ),
   ...CONSTANTS,
 });
