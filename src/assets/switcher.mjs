1412e2065d859421b0d42bad94441b5e836332fa
oti <otiext@gmail.com>
2023-05-18 15:13:38 +0900

    refactor: src/assets/switcher.mjs, class 化した

--- src/assets/switcher.mjs
+++ src/assets/switcher.mjs
@@ -1,29 +1,34 @@
-export const Switcher = () => {
-  const $Html = document.documentElement;
-  const $Mode = document.querySelector("#Mode");
-  const modes = [...$Mode.querySelectorAll(`[name="mode"]`)].map(
-    (v) => v.value
-  );
+export class Switcher {
+  constructor() {
+    try {
+      this.$Html = document.documentElement;
+      this.$Mode = document.querySelector("#Mode");
+      this.modes = [...this.$Mode.querySelectorAll(`[name="mode"]`)].map(
+        (v) => v.value
+      );
+      this.preferd = localStorage.getItem("prefer-accrefs-mode");
+      this.value = this.modes.includes(this.preferd)
+        ? this.preferd
+        : this.modes[0];
 
-  const updateMode = (value) => {
-    localStorage.setItem("prefer-accrefs-mode", value);
-    $Html.classList.remove(...modes);
-    $Html.classList.add(value);
-  };
-
-  const mountModeChanger = (value) => {
-    updateMode(value);
-
-    $Mode.querySelector(`[value="${value}"]`).setAttribute("checked", "");
-    $Mode.addEventListener("change", (e) => updateMode(e.target.value), false);
-  };
-
-  try {
-    const preferMode = localStorage.getItem("prefer-accrefs-mode");
-    const initMode = modes.includes(preferMode) ? preferMode : modes[0];
-    mountModeChanger(initMode);
-  } catch (e) {
-    console.error(e);
-    document.querySelector(".Mode").setAttribute("hidden", "");
+      this.update(this.value);
+      this.$Mode
+        .querySelector(`[value="${this.value}"]`)
+        .setAttribute("checked", "");
+      this.$Mode.addEventListener(
+        "change",
+        (e) => this.update(e.target.value),
+        false
+      );
+    } catch (e) {
+      console.error(e);
+      document.querySelector(".Mode").setAttribute("hidden", "");
+    }
   }
-};
+
+  update(value) {
+    localStorage.setItem("prefer-accrefs-mode", value);
+    this.$Html.classList.remove(...this.modes);
+    this.$Html.classList.add(value);
+  }
+}
