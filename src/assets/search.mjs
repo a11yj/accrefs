84c9e83b2ae65434d450e399a448050904a9b46e
oti <otiext@gmail.com>
2023-05-18 16:10:54 +0900

    refactor: src/assets/search.mjs, class 化した

--- src/assets/search.mjs
+++ src/assets/search.mjs
@@ -1,48 +1,54 @@
-export const Search = () => {
-  const $Search = document.querySelector("#Search");
-  if (!$Search) return;
-  let debounceTimer = 0;
-  const refs = [...document.querySelectorAll("article[id^=reference]")];
-  const models = refs.map((ref) => ({
-    id: ref.id,
-    title: ref.querySelector(".-reference").innerText.toLowerCase(),
-    link: ref.querySelector(".-reference").href.toLowerCase(),
-    year: ref.dataset.year,
-    content: ref.children[2] ? ref.children[1].innerText.toLowerCase() : null,
-  }));
+export class Search {
+  constructor() {
+    this.$Search = document.querySelector("#Search");
+    if (!this.$Search) return;
+    this.debounceTimer = 0;
+    this.refs = [...document.querySelectorAll("article[id^=reference]")];
+    this.models = this.refs.map((ref) => ({
+      id: ref.id,
+      title: ref.querySelector(".-reference").innerText.toLowerCase(),
+      link: ref.querySelector(".-reference").href.toLowerCase(),
+      year: ref.dataset.year,
+      content: ref.children[2] ? ref.children[1].innerText.toLowerCase() : null,
+    }));
 
-  const getMatchedIds = (lc_query) =>
-    models
+    this.$Search.addEventListener(
+      "input",
+      (e) => this.handleInput(e.target.value),
+      false
+    );
+    this.$Search.addEventListener("reset", () => this.handleReset(), false);
+  }
+
+  getMatchedIds = (query) =>
+    this.models
       .filter(
         ({ title, link, year, content }) =>
-          RegExp(lc_query).test(title) ||
-          RegExp(lc_query).test(link) ||
-          RegExp(lc_query).test(year) ||
-          RegExp(lc_query).test(content)
+          RegExp(query).test(title) ||
+          RegExp(query).test(link) ||
+          RegExp(query).test(year) ||
+          RegExp(query).test(content)
       )
       .map(({ id }) => id);
 
-  const search = (query) => {
-    const ids = getMatchedIds(query.toLowerCase());
-    refs.forEach((ref) =>
+  search = (query) => {
+    const ids = this.getMatchedIds(query.toLowerCase());
+    this.refs.forEach((ref) =>
       ids.includes(ref.id)
         ? ref.removeAttribute("hidden")
         : ref.setAttribute("hidden", "")
     );
   };
 
-  const reset = () => refs.forEach((ref) => ref.removeAttribute("hidden"));
+  reset = () => this.refs.forEach((ref) => ref.removeAttribute("hidden"));
 
-  const handleInput = (query) => {
-    if (debounceTimer) clearTimeout(debounceTimer);
-    debounceTimer =
+  handleInput = (query) => {
+    if (this.debounceTimer) clearTimeout(this.debounceTimer);
+    this.debounceTimer =
       query === ""
-        ? setTimeout(() => reset(), 1000 / 30)
-        : setTimeout(() => search(query), 1000 / 30);
+        ? setTimeout(() => this.reset(), 1000 / 30)
+        : setTimeout(() => this.search(query), 1000 / 30);
   };
 
-  const handleReset = () => reset();
-
-  $Search.addEventListener("input", (e) => handleInput(e.target.value), false);
-  $Search.addEventListener("reset", () => handleReset(), false);
-};
+  handleReset = () => this.reset();
+}
