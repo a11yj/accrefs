2e301aff6c144968a4a148bd9149139e91ec8436
oti <otiext@gmail.com>
2023-05-21 15:20:27 +0900

    change: task/matters.mjs, fast-glob に投げる引数を外から受け取れるように変更した

--- task/build.mjs
+++ task/build.mjs
@@ -3,4 +3,7 @@ import { database } from "./database.mjs";
 import { html } from "./html.mjs";
 import { matters } from "./matters.mjs";
 
-await assets().then(matters).then(database).then(html);
+await assets()
+  .then(async () => await matters("src/references/*.md"))
+  .then(database)
+  .then(html);
--- task/matters.mjs
+++ task/matters.mjs
@@ -3,9 +3,9 @@ import fg from "fast-glob";
 import matter from "gray-matter";
 import path from "path";
 
-export const matters = async () =>
+export const matters = async (glob) =>
   Promise.all(
-    (await fg("src/references/*.md")).map(async (filepath) => ({
+    (await fg(glob)).map(async (filepath) => ({
       id: path.parse(filepath).name,
       ...matter(await fs.readFile(filepath, "utf8")),
     }))
